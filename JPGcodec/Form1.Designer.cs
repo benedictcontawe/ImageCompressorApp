a3b27ec291f478e1e5ac004323107f36eea9c38b
Benedict <benedictcontawe@gmail.com>
2019-07-07 00:08:06 +0800

    Add project files.

---
+++ JPGcodec/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace JPGcodec
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtFilePthOrig = new System.Windows.Forms.TextBox();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtFilePthOrig
+            // 
+            this.txtFilePthOrig.Location = new System.Drawing.Point(12, 12);
+            this.txtFilePthOrig.Name = "txtFilePthOrig";
+            this.txtFilePthOrig.ReadOnly = true;
+            this.txtFilePthOrig.Size = new System.Drawing.Size(335, 22);
+            this.txtFilePthOrig.TabIndex = 0;
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Location = new System.Drawing.Point(272, 40);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(75, 23);
+            this.btnBrowse.TabIndex = 1;
+            this.btnBrowse.Text = "Browse";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(12, 40);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(172, 23);
+            this.btnSave.TabIndex = 2;
+            this.btnSave.Text = "Save and Compress";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Location = new System.Drawing.Point(12, 69);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(335, 305);
+            this.pictureBox.TabIndex = 3;
+            this.pictureBox.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(359, 386);
+            this.Controls.Add(this.pictureBox);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnBrowse);
+            this.Controls.Add(this.txtFilePthOrig);
+            this.Name = "Form1";
+            this.Text = "JPG Compression with Codec";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtFilePthOrig;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.PictureBox pictureBox;
+    }
+}
+
---
+++ JPGcodec/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Drawing.Imaging;
+
+namespace JPGcodec
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //CompressImage(Image.FromFile(Application.StartupPath + @"\mySourcePicture.jpg"), 30, Application.StartupPath + @"\myCompressedPicture.jpg");
+            //CompressImage(Image.FromFile(@"C:\Users\Benedict\Pictures\Saved Pictures\SoftenFilter.jpg"), 30, @"C:\Users\Benedict\Pictures\Saved Pictures\MeSoftenFilterCompression.jpg");
+        }
+        private void CompressImage(Image sourceImage, int imageQuality, string savePath)
+        {
+            try
+            {
+                //Create an ImageCodecInfo-object for the codec information
+                ImageCodecInfo jpegCodec = null;
+
+                //Set quality factor for compression
+                EncoderParameter imageQualitysParameter = new EncoderParameter(System.Drawing.Imaging.Encoder.Quality, imageQuality);
+
+                //List all avaible codecs (system wide)
+                ImageCodecInfo[] alleCodecs = ImageCodecInfo.GetImageEncoders();
+
+                EncoderParameters codecParameter = new EncoderParameters(1);
+                codecParameter.Param[0] = imageQualitysParameter;
+
+                //Find and choose JPEG codec
+                for (int i = 0; i < alleCodecs.Length; i++)
+                {
+                    if (alleCodecs[i].MimeType == "image/jpeg")
+                    {
+                        jpegCodec = alleCodecs[i];
+                        break;
+                    }
+                }
+
+                //Save compressed image
+                sourceImage.Save(savePath, jpegCodec, codecParameter);
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Title = "Select an image file.";
+            ofd.Filter = "Jpeg Images(*.jpg)|*.jpg";
+            ofd.Filter += "|Png Images(*.png)|*.png";
+            ofd.Filter += "|Bitmap Images(*.bmp)|*.bmp";
+            ofd.Filter += "|All(*.JPG, *.PNG, *.bmp)| *.JPG; *.PNG; *.bmp";
+            ofd.FilterIndex = 1;
+
+            if (ofd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                txtFilePthOrig.Text = ofd.FileName;
+                Image img = Image.FromFile(txtFilePthOrig.Text);
+                pictureBox.Image = img;
+                pictureBox.SizeMode = PictureBoxSizeMode.StretchImage;
+            }
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            if (txtFilePthOrig.Text == "")
+            {
+                MessageBox.Show("Please load image first!");
+            }
+            else if (txtFilePthOrig.Text.Contains(".jpg")) 
+            {
+                CompressImage(Image.FromFile(txtFilePthOrig.Text), 30, txtFilePthOrig.Text.Insert(txtFilePthOrig.Text.Length - 4, " JPEG Compressed Image"));
+                MessageBox.Show("Image compressed!");
+            }
+            else
+            {
+                string x = txtFilePthOrig.Text.Insert(txtFilePthOrig.Text.Length - 4, " JPEG Compressed Image");
+                string y = "abcdefg";
+                //x.Substring(0, x.Length-4)+".jpg"
+                //MessageBox.Show(txtFilePthOrig.Text.Insert(txtFilePthOrig.Text.Length - 4, " Compressed Image").Substring(0, x.Length - 4) + ".jpg");
+
+                CompressImage(Image.FromFile(txtFilePthOrig.Text), 30, txtFilePthOrig.Text.Insert(txtFilePthOrig.Text.Length - 4, " JPEG Compressed Image").Substring(0, x.Length - 4) + ".jpg");
+                MessageBox.Show("Image compressed!");
+            }
+        }
+    }
+}
